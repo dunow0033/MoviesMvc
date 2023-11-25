16231d164124417fdd745bbe4be9516aa1029b97
Brian Donnoe <dunow33@hotmail.com>
2023-11-24 21:03:34 -0500

    making progress on adding a movie

--- MoviesMvc/Controllers/MoviesController.cs
+++ MoviesMvc/Controllers/MoviesController.cs
@@ -1,17 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoviesMvc.Models;
+using MoviesMvc.Data;
 
 namespace MoviesMvc.Controllers;
 
 public class MoviesController : Controller
 {
-	public IActionResult Index()
-	{
-		return View();
-	}
+    private readonly MoviesDbContext _context;
 
-	[HttpGet]
-	public IActionResult Create()
-	{
-		return View();
-	}
+    public MoviesController(MoviesDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Movie movie)
+    {
+        var newMovie = new Movie()
+        {
+            Title = movie.Title,
+            Release_date = movie.Release_date,
+            Genre = movie.Genre,
+            Price = movie.Price,
+            Rating = movie.Rating,
+        };
+
+        _context.Movies.Add(newMovie);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
 }
